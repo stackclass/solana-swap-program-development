9453e5de90a0581672272c3ec2305b33c16ae545
lispking <lispking@qq.com>
2025-12-29 20:44:56 +0800

    Add template configuration and build scripts
    - Add .eslintrc.js for ESLint configuration
    - Add .stackclass/ directory with compile and run scripts
    - Add stackclass.yml for course configuration
    - Add your_program.sh as program entry point

---
+++ template/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  ignorePatterns: ["**/target/**", "node_modules"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/ban-ts-comment": "off",
+  },
+};
